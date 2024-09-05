418156d2784d5b186839e533fc422752655ac06b
Amanda <22.00986-8@maua.br>
2024-09-05 16:42:40 -0300

    inicio curso Oracle

---
+++ oracle/JP1.java
@@ -0,0 +1,23 @@
+import inputVariables;
+import java.util.Scanner;
+
+
+
+public class JP1 {
+    public static void main(String[] args) {
+        int iNum = 25;
+        char cVal = 'B';
+        boolean bVla = true;
+
+        System.out.println(iNum);
+        System.out.println(cVal);
+        System.out.println(bVla);
+
+
+        String str1 = "Hello";
+        String str2 = "Amanda";
+        String str3 ;
+
+        System.out.println(str1.equals(str2));
+    }
+}
