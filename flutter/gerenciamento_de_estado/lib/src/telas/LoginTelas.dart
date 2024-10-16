ded2c2a3490021ae223b789ffb93fcc71fcee013
Amanda <22.00986-8@maua.br>
2024-10-16 11:45:09 -0300

    gerenciamento de estados ate 32

--- flutter/gerenciamento_de_estado/lib/src/telas/LoginTelas.dart
+++ flutter/gerenciamento_de_estado/lib/src/telas/LoginTelas.dart
@@ -9,17 +9,53 @@ class LoginTelas extends StatelessWidget{
         children: [
           Text ("Filho1"),
           Text ("Filho2"),
+          emailField(),
+          passwordField(),
+          submitButton()
         ],
         ),
     );
   }
-
-  Widget emailField(){
-  return TextField();
-    decoration: InputDecoration(
-      labelText: 'Email',
-      hintText: ''
+Widget emailField(){
+    //escolher o tipo do teclado, apropriado para email
+    //exibir um texto de "dica" para o usuário, dizendo a ele que ele deve digitar um email
+    return TextField();
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'seu@email.com',
+        labelText: 'Endereço de e-mail'
+      ),
+    );
+  }
+  //escrever novo método que produz um campo próprio para senha
+  Widget passwordField(){
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Senha',
+        labelText: 'Senha'
+      ),
+    );
+  }
+  //escrever novo método que produz um botão
+  Widget submitButton(){
+    return Container(
+      margin: EdgeInsets.only(top: 12.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: ElevatedButton(
+                onPressed: (){}, 
+                child: Text('Login')
+            ),
+          )
+        ],
+      )
+      
+      
+      
+    );
   }
 }
-
 
