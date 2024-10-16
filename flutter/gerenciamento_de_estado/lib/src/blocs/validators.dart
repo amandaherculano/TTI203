844e1d27dcde17a0e9368785f93758e9822920b3
Amanda <22.00986-8@maua.br>
2024-10-16 12:41:02 -0300

    gerenciamento de estados errorText

--- flutter/gerenciamento_de_estado/lib/src/blocs/validators.dart
+++ flutter/gerenciamento_de_estado/lib/src/blocs/validators.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:email_validator/email_validator.dart';
 mixin Validators{
   final validateEmail = StreamTransformer<String, String>.fromHandlers(
     handleData: (email, sink){
@@ -6,8 +7,28 @@ mixin Validators{
       //usando o EmailValidator, verifique se o e-mail é válido
       //se for válido, apenas adicione o e-mail ao sink
       //caso contrário adicione um ERRO ao sink com a mensagem "E-mail inválido"
+      if (EmailValidator.validate(email)){
+        sink.add(email);
+      } else {
+        sink.addError('E-mail inválido');
+      }
       
     }
   );
+  final validatePassword = StreamTransformer<String, String>.fromHandlers(
+    handleData: (password, sink){
+      //faça um if/else
+      //verifique se a senha tem pelo menos 6 caracteres
+      //se tiver, adicione a senha ao sink
+      //caso contrário, adicione um ERRO ao sink com a mensagem "Senha deve ter pelo menos 4 caracteres"
+      if (password.length >= 4){
+        sink.add(password);
+      } else {
+        sink.addError('Senha deve ter pelo menos 4 caracteres');
+      }
+    }
+  );
+
+
 }
 //visitar o pub.dev e encontrar um pacote apropriado para a validação de e-mail
--- flutter/gerenciamento_de_estado/lib/src/telas/LoginTelas.dart
+++ flutter/gerenciamento_de_estado/lib/src/telas/LoginTelas.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../blocs/blocs.dart';
 
 class LoginTelas extends StatelessWidget{
   @override
@@ -17,16 +18,33 @@ class LoginTelas extends StatelessWidget{
     );
   }
 Widget emailField(){
+
+  return StreamBuilder(
+    stream: bloc.email, //em emial esta implicito o transform
+    builder: (context, snapshot){
+      return TextField(
+        onChanged: (newValue){
+          bloc.changeEmail(newValue);
+        },
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          hintText: 'seu@email.com',
+          labelText: 'Endereço de e-mail',
+          //acessar a propriedade error text e associar a ela o erro que vem do snapshot 
+          // errorText: snapshot.error.toString()
+          // exibir o erro de maneira condicional, só exibe se tem erro e com ternario 
+          errorText: snapshot.hasError ? snapshot.error.toString() : null
+
+      )
+
+      
+
+
+      );
+     }
     //escolher o tipo do teclado, apropriado para email
     //exibir um texto de "dica" para o usuário, dizendo a ele que ele deve digitar um email
-    return TextField();
-    return TextField(
-      keyboardType: TextInputType.emailAddress,
-      decoration: InputDecoration(
-        hintText: 'seu@email.com',
-        labelText: 'Endereço de e-mail'
-      ),
-    );
+  );
   }
   //escrever novo método que produz um campo próprio para senha
   Widget passwordField(){
