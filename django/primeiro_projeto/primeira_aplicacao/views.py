a25ea5bde6827368406ebc89ff9a808e34e464c9
Amanda <22.00986-8@maua.br>
2024-11-13 12:01:46 -0300

    teste

--- django/primeiro_projeto/primeira_aplicacao/views.py
+++ django/primeiro_projeto/primeira_aplicacao/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
+# hirosnfiofd
+# nao especificou: implicito get
 def index(request):
     return HttpResponse("Hello, Django!")
 
